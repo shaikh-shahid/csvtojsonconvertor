67b702dd0a5a63e65c057fdea01687e1fce7a376
Shahid Shaikh <UnixRoot@Shahids-MacBook-Air.local>
2017-02-10 17:33:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const csv = require('csvtojson');
+const router = express.Router();
+const fs = require('fs');
+
+router.get('/',(req,res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+router.get('/convert/csv/to/json',(req,res) => {
+  if(req.query.q === undefined || req.query.q === null || req.query.q === '') {
+    return res.json({error: true,message: 'No file provided!'});
+  }
+  request(req.query.q,(error,response,body) => {
+    if(error) {
+      return res.json({error: true,message: 'Error occurred reading CSV file!'});
+    }
+    let jsonData = [];
+    csv()
+      .fromString(response.body)
+      .on('json',(data) => {
+        console.log(data);
+        jsonData.push(data);
+      })
+      .on('done',() => {
+        res.json({error: false,message: "Success", data: jsonData});
+      });
+  });
+});
+
+app.use(express.static('js'));
+app.use('/',router);
+
+app.listen(3000,() => {
+  console.log("Listening to Port 3000");
+});
